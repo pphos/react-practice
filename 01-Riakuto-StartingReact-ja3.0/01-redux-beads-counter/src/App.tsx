ccb4a893daa38c3d307c830e9eed1aa0ee373672
pphos <coc4h8conhc6h12nh@gmail.com>
2021-03-27 20:55:36 +0900

    init 01-redux-beads-counter

---
+++ 01-Riakuto-StartingReact-ja3.0/01-redux-beads-counter/src/App.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+const BULK_UNIT = 10;
+
+const App: React.FC = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const add = (amount: number) => {
+    setCount(count => count + amount);
+  }
+  const decrement = () => {
+    setCount(count - 1);
+  };
+  return (
+    <div className="container mx-auto mt-10">
+      <div className="flex">
+        <div className="p-4 md:w-1/3 mx-auto">
+          <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg">
+            <div className="flex flex-col items-center">
+              <h1 className="title-font text-2xl font-medium text-gray-600">
+                ビーズカウンター
+              </h1>
+              <p className="font-medium text-6xl text-gray-800 mb-3">
+                {count}
+              </p>
+            </div>
+            <div className="grid grid-cols-2 px-3">
+              <button
+                className="text-white bg-red-500 rounded text-lg focus:outline-none hover:bg-red-700 p-2"
+                onClick={decrement}
+              >-1</button>
+              <button
+                className="text-white bg-green-500 rounded text-lg focus:outline-none hover:bg-green-700"
+                onClick={increment}>+1</button>
+              <button
+                className="col-start-1 col-end-3 text-white bg-gray-500 rounded text-lg focus:outline-none hover:bg-gray-700 my-3 p-2"
+                onClick={() =>add(BULK_UNIT)}>+{BULK_UNIT}</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ 01-Riakuto-StartingReact-ja3.0/README.md
@@ -0,0 +1,8 @@
+# 『りあクト！ TypeScript で始めるつらくない React 開発 第3版』を読み進める。
+公式リポジトリ: https://github.com/oukayuka/Riakuto-StartingReact-ja3.0
+
+## 販売ページリンク (BOOTH)
+- [くるみ割書房 - BOOTH](https://oukayuka.booth.pm/)
+  - [Ⅰ. 言語・環境編（電子版 / 218p / ￥1,200）](https://booth.pm/ja/items/2368045)
+  - [Ⅱ. React基礎編（電子版 / 198p / ￥1,200）](https://booth.pm/ja/items/2368019)
+  - [Ⅲ. React応用編（電子版 / 211p / ￥1,200）](https://booth.pm/ja/items/2367992)
